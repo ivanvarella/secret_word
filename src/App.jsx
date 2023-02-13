8f1834a95d3320f594128c616a9619ebe94d4765
Ivan Varella <ivanvarella@gmail.com>
2023-02-13 12:15:54 -0300

    Generate random category and word + init states

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,33 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState("");
+
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { category, word };
+  };
+
   const startGame = () => {
+    const { category, word } = pickWordAndCategory();
+    console.log("Category: ", category, " - Word: ", word);
+
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+    console.log(wordLetters);
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
