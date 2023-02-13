0fbf999924b81126510f3e75d8e7b5c48ddb3bbe
Ivan Varella <ivanvarella@gmail.com>
2023-02-13 10:50:38 -0300

    clean code + WIP: gameStage logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+/* CSS */
 import "./App.css";
+
+/* React */
+import { useCallback, useEffect, useState } from "react";
+
+/* Data */
+import { wordsList } from "./data/words";
+
+/* Components */
 import StartScreen from "./components/StartScreen/StartScreen";
 
+const stages = [
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
+
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === "start" && <StartScreen />}
+      {gameStage === "game" && <StartScreen />}
+      {gameStage === "end" && <StartScreen />}
     </div>
   );
 }
